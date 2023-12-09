1b257f175eab9600f802115b39369b3ed1e3ff62
Abhijeet  Kumar <32939445+Abhijeet-Tiwari@users.noreply.github.com>
2023-12-09 20:04:57 +0530

    Installed react and react-dom

--- Assignment 02- Ignite Our App/App.js
+++ Assignment 02- Ignite Our App/App.js
@@ -1,31 +1,10 @@
-//Hello World using React
-
-/* 
-  const heading = React.createElement('h1', {}, 'Hello World using React!');
-  console.log(heading) // This returns an Object
-  const root = ReactDOM.createRoot(document.getElementById('root'));
-  root.render(heading); 
-*/
-
-// Create Nested HTML structure
-
-/* 
-<div id="parent">
-    <div id="child">
-        <h1>I'm h1 tag</h1>
-        <h2>I'm h2 tag</h2>
-    </div>
-    <div id="child2">
-        <h1>I'm h1 tag</h1>
-        <h2>I'm h2 tag</h2>
-    </div>
-</div> 
-*/
+import React  from 'react';
+import ReactDOM from 'react-dom/client';
 
 const parent = React.createElement('div', {id: 'parent'}, [
   React.createElement('div', {id: 'child1'}, 
   [
-    React.createElement('h1', {}, "I'm h1 tag"),
+    React.createElement('h1', {}, "This is Namaste React 🤖"),
     React.createElement('h2', {}, "I'm h2 tag")
   ]),
     React.createElement('div', {id: 'child2'}, [
